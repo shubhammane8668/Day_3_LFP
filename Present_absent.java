c01470e4f95d25c619cd47e47f59c3bd476a6832
shubhammane8668 <shubhsmane@gmail.com>
2022-09-16 18:32:32 +0530

    Added programe forpresenr or absent

---
+++ Present_absent.java
@@ -0,0 +1,15 @@
+package Employee_wage;
+
+public class Present_absent {
+
+	public static void main(String[] args) {
+         int IS_FULL_TIME=1;
+		
+		double empcheck =Math.floor(Math.random()*10)%2;
+		if (empcheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+
+}
