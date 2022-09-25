74bf93dbe467a9a66d37bb48366fcb6db938f5ec
shubhammane8668 <shubhsmane@gmail.com>
2022-09-25 13:24:10 +0530

    Used switch case

--- Uc_5.java
+++ Uc_5.java
@@ -1,13 +1,14 @@
 package Employee_wage;
 
-public class Uc_4 {
-  
-	/*
-	 * With the help of switchcase
-	 */
+public class Uc_5 {
+/*
+ * calculating wages for a month
+ * assume 20 wok 
+ */
 public static final	int IS_PART_TIME = 1;	
 public static final	int IS_FULL_TIME = 2;
-public static final	int EMP_RATE_PER_HOUR=20;
+public static final	int EMP_RATE_PER_HOUR = 20;
+public static final	int NUM_OF_WORKING_DAYS = 20;
 	
 
 	public static void main(String[] args) {
@@ -18,11 +19,12 @@ public static final	int EMP_RATE_PER_HOUR=20;
 	 */
 	int emphrs = 0;
 	int empWage =0;
-	 
+	int totalempwage =0;
     /*
      * computation
 	 */
-	int empcheck =(int) (Math.floor(Math.random()*10)%3) ;
+	for (int day =0; day<NUM_OF_WORKING_DAYS; day++) {
+	   int empcheck =(int) (Math.floor(Math.random()*10)%3) ;
 	/*
 	 * using switchcase
 	 */
@@ -36,9 +38,12 @@ public static final	int EMP_RATE_PER_HOUR=20;
 		break;
 		
 		default:
-			        emphrs= 0;
+			        emphrs= 0; 
 		}
 		    empWage = emphrs * EMP_RATE_PER_HOUR;
+		    totalempwage += empWage;
 		System.out.println("EmpWage :" + empWage );	
-	}
+		}
+	System.out.println("Total Emp Wage: " +  totalempwage);	
+   }
 }
